1f914f0c9da9f004f816c8c6d782d29129c8e427
vlad_vosov <vlad.shvetesov@gmail.com>
2023-05-20 17:20:35 +1000

    add new function and add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def uppercase(line):
+    """Переводит строчку из нижнего реистра в ВЕРХНИЙ"""
     return line.upper()
+
+def uppercase_first_letter(line):
+    """Переводит первую букву каждого слова в ВЕРХНИЙ реистр"""
+    return line.title()
